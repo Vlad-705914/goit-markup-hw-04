1625701c2ed217af9819a1912d258e75c99d2ac5
Vlad-705914 <83553237+Vlad-705914@users.noreply.github.com>
2022-01-28 17:49:27 +0200

    test

---
+++ js/speacer.js
@@ -0,0 +1,12 @@
+(() => {
+    document
+        .guerySelector('.js-speaker-form')
+        .addEventListener('submit', e => {
+            e.preventDefault();
+
+            new FormData(e.currentTarget).forEach(value, name) =>
+            console.log(`${name}: ${value}`),
+    );
+    e.currentTarget.reset();
+});
+}) ();
